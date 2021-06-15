bc167df6e77b8edfbed68676cc43bbacbe0dd2dc
ttyler <ttyler@mac.com>
2021-06-15 09:13:41 +0700

    Update downloadInstallFromURL.sh
    CHANGELOG:
    1. Added support for second URL to download Apple Silicon versions of software if seperate to intel (like google chrome is doing right now)
    2. Added platform detection to install arm64 versions if the above URL is set
    3. Made logging a bit nicer, before it was very janky, now its only moderately janky
    4. Finally put some basic checks in to see if variables were empty before trying to download stuff
    5. Re-arranged the variable numbering.. Caution, this will break drop in replacements if updating this script with existing polices.

--- downloadInstallFromURL.sh
+++ downloadInstallFromURL.sh
@@ -8,6 +8,10 @@
 ##
 ## No warranty is implied or supplied, and all use is stricly at your own risk.
 ## You can make any edits and redistributre at your own free will. just give me some credit.
+##
+## Now updated with dual URL support for M1 Processors, You can provide a URL for normal downloads
+## Which is required, and an Alt URL for M1 software versions. SCript will detect which version
+## To download and install based on local processor type.
 ###########################################################################################
 
 ###########################################################################################
@@ -27,6 +31,7 @@
 ## Lets generate a UUID so we have a unique id for un-important element names ( no need to change )
 uniqueID=$(uuidgen)
 
+
 ## Lets log it all to a file, but where?
 logfile="/Library/Management/Logs/ScriptInstallScript.log"
 
@@ -35,25 +40,48 @@ logfile="/Library/Management/Logs/ScriptInstallScript.log"
 ## EXAMPLE: 'https://com.file.com/file.dmg'
 url="$4"
 
+## This is the URL to pull Apple Silicon Versionf from, should be $5 if using jamf
+## This is optional as universal URLs and Rosetta are a thing
+## NOTE if manually hardcoing a URL here please use single quotes around the url
+## EXAMPLE: 'https://com.file.com/file.dmg'
+as_url="$5"
+
 ## What type of file are we expecting to download. dmg or pkg, 
-## should be pulled as $5 if using jamf
-type="$5"
+## should be pulled as $6 if using jamf
+type="$6"
 
 ################################# Dodgey Hacks! ####################################################
 
 ## Special comand to install ( Dont use this normally ) 
-## But you can specify the exact command required to perform this install action once the DMG is 
+## But you can specifiy the exact command required to perform ths install action once the DMG is 
 ## downloaded and mounted. 
 ## For example, when you download flash player you need to execute an installer pkg that is 
 ## embeded inside an app bundle, inside the DMG itself
-##. So you could run --
-##. installer -pkg "/Volumes/Flash Player/Install Adobe Flash Player.app/Contents/Resources/Adobe Flash Player.pkg" -target /
+##. So you run --
+##. /usr/sbin/installer -pkg "/Volumes/Flash Player/Install Adobe Flash Player.app/Contents/Resources/Adobe Flash Player.pkg" -target /
 ##
 ## So here is where put the whole command in the special variable.
-## Leave this blank unless required and if required, use variable 6 with jamf
-special="$6"
+## Leave this blank unless required and if required, use variable 7 with jamf
+special="$7"
+
+
+################################# Quick Checks! ###############################################
+
+if [ -z "${url}" ]; then
+	echo "No URL variable specified to download files from, kinda need that."
+	echo "Read script comments for more usage information"
+	exit 1
+fi
+
+if [ -z "${type}" ]; then
+	echo "No Package Type variable specified to install, kinda need that."
+	echo "Read script comments for more usage information"
+	exit 1
+fi
 
 ################################# Action! ####################################################
+/bin/echo "`date`: -----------------" >> ${logfile}
+/bin/echo "`date`: Starting.." >> ${logfile}
 
 ## Make a file name out of our uuid and file type
 dmgfile="${uniqueID}.${type}"
@@ -63,15 +91,39 @@ if [[ ! -d "/Library/Management/Logs/" ]]; then
 	mkdir -p "/Library/Management/Logs/"
 fi
 
-## log some header crap to the file
+## log some header stuff to the file
 /bin/echo "--" >> ${logfile}
-/bin/echo "`date`: Downloading latest version." >> ${logfile}
+/bin/echo "`date`: Downloading source files now." >> ${logfile}
 
-## Download the file using curl ( flat files only )
-## We try to use generic links where possible, and follow the links with the L arg
-/usr/bin/curl -k -L -s -o "/tmp/${dmgfile}" ${url}
+## See if we have passed an Apple Silicon link as an alternative dowload
+if [ ! -z "${as_url}" ]; then
+	/bin/echo "`date`: Alternate URL for Apple Silicon passed to script, checking local hardware." >> ${logfile}
+	## Get the machine arch to see if we have apple silicon
+	arch=$(/usr/bin/arch)
+	
+	## If we have Apple Silcion download from ALT URL 
+	if [ "${arch}" == "arm64" ]; then
+		/bin/echo "`date`: Apple silicon found, downloading from ${as_url}." >> ${logfile}
+		## Download the file for Apple Silcion using curl ( flat files only )
+		## We try to use generic links where possible, and follow the links with the L arg
+		/usr/bin/curl -k -L -s -o "/tmp/${dmgfile}" ${as_url}
+	
+	## If its not, then assume intel x86 and download from main URL
+	else
+		/bin/echo "`date`: non arm64 processor found, downloading from ${url}." >> ${logfile}
+		## Download the file using curl ( flat files only )
+		## We try to use generic links where possible, and follow the links with the L arg
+		/usr/bin/curl -k -L -s -o "/tmp/${dmgfile}" ${url}
+	fi
+else
+	/bin/echo "`date`: Platform agnostic download from ${url}." >> ${logfile}
+	## Download the file using curl ( flat files only )
+	## We try to use generic links where possible, and follow the links with the L arg
+	/usr/bin/curl -k -L -s -o "/tmp/${dmgfile}" ${url}
+fi
 
-/bin/echo "`date`: Installing ${dmgfile} ..." >> ${logfile}
+
+/bin/echo "`date`: Attempting install from ${dmgfile} ..." >> ${logfile}
 
 ## Some apps have spaces in the name.. so here you go.
 IFS=$'\n'
@@ -89,17 +141,20 @@ if [[ "${type}" == "dmg" ]]; then
 			
 			## found an app bundle, lets copy it to applications folder
 			if [[ "$i" = *.app ]]; then
+				/bin/echo "`date`: Copying files to Applications Folder" >> ${logfile}
 				ditto -rsrc "${volname}/$i" "/Applications/$i"
 			fi
 			
 			## Inside the DMG we found a PKG, how nice of them. lets install it
 			if [[ "$i" = *.pkg ]]; then
+				/bin/echo "`date`: Installing PKG from inside of the DMG" >> ${logfile}
 				installer -pkg "${volname}/$i" -target /
 			fi
 		done
 	else
 		## So there was something specified in special.. lets chuck that at the wall and
 		## see if its sticks.
+		/bin/echo "`date`: Special install - Executing  ${special}" >> ${logfile}
 		bash -c "${special}"
 	fi	
 	
@@ -117,3 +172,6 @@ fi
 
 ## Clean up		
 /bin/rm /tmp/"${dmgfile}"
+
+/bin/echo "`date`: Install completed" >> ${logfile}
+/bin/echo "`date`: -----------------" >> ${logfile}
